1c108f85a8554bf72329a4aa3d45d5974beeb463
Dilyara Fatykhova <72702419+diquid@users.noreply.github.com>
2023-04-08 00:57:39 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,12 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 401164404 # Ваш chat ID, не меняйте название переменной
+chat_id = 1234206085
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    # return x.mean() / 100  # Ваш ответ
-    x = 2 * x / (100 ** 2)
-    return x.mean()
+    return np.log(x - 451).mean()
